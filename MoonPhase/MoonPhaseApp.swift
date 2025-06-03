90d9db398d091fd63fcd250cbc2b09378a33301f
jurek <jerzyzerdzinski@gmail.com>
2025-06-03 15:15:34 +0200

    Initial Commit

---
+++ MoonPhase/MoonPhaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonPhaseApp.swift
+//  MoonPhase
+//
+//  Created by Jerzy Żerdziński on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonPhaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
